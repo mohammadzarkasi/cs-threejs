b5d7ef0fc6cf17f66972782ff498451ec85d45b3
mozar <mozar@mozars-MacBook-Pro.local>
2025-10-26 14:56:27 +0700

    menggambar objek dari cs

--- Pages/Home2.razor.cs
+++ Pages/Home2.razor.cs
@@ -9,13 +9,60 @@ public partial class Home2 : ComponentBase, IAsyncDisposable
     [Inject] private IJSRuntime JSRuntime { get; set; } = null!;
     private string threeJsCdnUrl = "https://cdnjs.cloudflare.com/ajax/libs/three.js/r134/three.min.js";
 
+    private List<int> cubes = [];
+
     protected override async Task OnAfterRenderAsync(bool firstRender)
     {
         if (firstRender)
         {
-             await JSRuntime.InvokeVoidAsync("ScriptInjector.injectScript", "threejs-cdn", threeJsCdnUrl);
-             await JSRuntime.InvokeVoidAsync("ScriptInjector.injectScript", "my-threejs", "js/my-threejs.js");
-             await JSRuntime.InvokeVoidAsync("MyThree.Init");
+            var ts = DateTime.UtcNow;
+            await JSRuntime.InvokeVoidAsync("ScriptInjector.injectScript", "threejs-cdn", threeJsCdnUrl);
+            await JSRuntime.InvokeVoidAsync("ScriptInjector.injectScript", "my-threejs", "js/my-threejs.js?ts=" + ts.ToString("o"));
+            await JSRuntime.InvokeVoidAsync("MyThree.Init", new {
+                camera = new {
+                    position = new {
+                        x = 0,
+                        y = 1,
+                        z = 15,
+                    },
+                },
+                objects = new List<object>(){
+                    new { 
+                        type = "cube",
+                        args = new {
+                            size = new {
+                                x = 1,
+                                y = 2,
+                                z = 3,
+                            },
+                            position = new {
+                                x = -2,
+                                y = 0,
+                                z = 1
+                            },
+                            material = "meshbasic",
+                            color = 0xff0000
+                        },
+                    },
+                    new { 
+                        type = "cube",
+                        args = new {
+                            size = new {
+                                x = 1,
+                                y = 1.2,
+                                z = 5
+                            },
+                            position = new {
+                                x = 3,
+                                y = 2,
+                                z = -1
+                            },
+                            material = "meshbasic",
+                            color = 0x00ff00
+                        },
+                    }
+                }
+            });
         }
         //return base.OnAfterRenderAsync(firstRender);
     }
--- wwwroot/js/my-threejs.js
+++ wwwroot/js/my-threejs.js
@@ -1,12 +1,16 @@
 window.MyThree = {
+    myScene: null,
+    myCamera: null,
     Dispose: function() {
         console.log("MyThree dispose");
     },
-    Init: function() {
+    Init: function(params) {
         console.log("MyThree initialize"); 
         const scene = new THREE.Scene();
+        this.myScene = scene;
 
         const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+        this.myCamera = camera;
 
         const renderer = new THREE.WebGLRenderer();
         renderer.setSize( window.innerWidth-210, window.innerHeight-160 );
@@ -15,18 +19,62 @@ window.MyThree = {
         // container.append(renderer);
         container.appendChild(renderer.domElement);
 
-        const geometry = new THREE.BoxGeometry( 1, 1, 1 );
-        const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-        const cube = new THREE.Mesh( geometry, material );
-        scene.add( cube );
-
+        // const geometry = new THREE.BoxGeometry( 1, 1, 1 );
+        // const material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+        // const cube = new THREE.Mesh( geometry, material );
+        // scene.add( cube );
+        
         camera.position.z = 5;
 
+        if(params){
+            if(params.objects){
+                this.AddObjects(params.objects);
+            }
+            
+            const cam = params.camera;
+            console.log('camera params', cam);
+            const pos = cam?.position;
+            camera.position.set(pos?.x ?? 0, pos?.y ?? 0, pos?.z ?? 5);
+            
+        }
+
+        
+
         function animate() {
-            cube.rotation.x += 0.01;
-            cube.rotation.y += 0.01;
+            // cube.rotation.x += 0.01;
+            // cube.rotation.y += 0.01;
             renderer.render( scene, camera );
         }
         renderer.setAnimationLoop( animate );
+    },
+    AddObjects: function(objects){
+        for(var obj of objects){
+            console.log(obj);
+            if(obj.type == 'cube'){
+                this.AddCube(obj.args);
+            }
+        }
+    },
+    AddCube: function(params){
+        // console.log('add cube', params);
+        // console.log('add cube', typeof params);
+        if(params == undefined || params == null){
+            return;
+        }
+        const size = params.size;
+        const geometry = new THREE.BoxGeometry( size?.x ?? 1, size?.y ?? 1, size?.z ?? 1 );
+        const material = this.CreateMaterial(params);
+        const cube = new THREE.Mesh( geometry, material );
+        const position = params.position;
+        cube.position.set(position?.x ?? 0, position?.y ?? 0, position?.z ?? 0);
+        console.log('new cube', cube);
+        this.myScene.add( cube );
+
+        return cube.id;
+    },
+    CreateMaterial: function(params){
+        if(params.material == 'meshbasic'){
+            return new THREE.MeshBasicMaterial({color: params.color ?? 0xffff00 });
+        }
     },
 };
